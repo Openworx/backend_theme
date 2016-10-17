39a7799a205fcbe14ef15078c5e9fa43a308df6f
root <root@odoo10-panel.lan>
2016-10-17 17:30:51 -0400

    Several changes

--- backend_theme_v10/__openerp__.py
+++ backend_theme_v10/__openerp__.py
@@ -11,8 +11,11 @@
     "website": "http://www.openworx.nl",
 	"description": """
 Backend theme for Odoo 10.0 community edition.
-The app dashboard is based on the module web_responsive from LasLabs Inc.
+The app dashboard is based on the module web_responsive from LasLabs Inc and the theme on Bootstrap United.
     """,
+	'images':[
+        'images/screen.png'
+	],
     "author": "Openworx",
     "license": "LGPL-3",
     "installable": True,
