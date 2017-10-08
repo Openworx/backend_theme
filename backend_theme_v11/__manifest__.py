d0fdfbdd458a92664e7008cf61f3b0e95e568673
root <root@odoo11-yenthe-20171004.lan>
2017-10-08 16:19:28 -0400

    First commit

---
+++ backend_theme_v11/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016, 2017 Openworx
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Material/United Backend Theme",
+    "summary": "Odoo 11.0 community backend theme",
+    "version": "11.0.1.0.0",
+    "category": "Themes/Backend",
+    "website": "http://www.openworx.nl",
+	"description": """
+		Backend theme for Odoo 11.0 community edition.
+    """,
+	'images':[
+        'images/screen.png'
+	],
+    "author": "Openworx",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+	'web_responsive',
+    ],
+    "data": [
+        'views/assets.xml',
+    ],
+}
+
