b4a8ad4d6258df1842a8f5e87bf5f3d53a90b752
root <root@odoo12-dev.lan>
2018-11-01 12:24:23 +0000

    First release for Odoo 12 CE.

---
+++ openworx_backend_theme/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016, 2018 Openworx
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Openworx Backend Theme",
+    "summary": "Openworx Backend Theme 	",
+    "version": "12.0.0.1",
+    "category": "Themes/Backend",
+    "website": "http://www.openworx.nl",
+	"description": """
+		Backend theme for Odoo 12.0 community edition.
+    """,
+	'images':[
+        'images/screen.png'
+	],
+    "author": "Openworx",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+    ],
+    "data": [
+        'views/assets.xml',
+        'views/sidebar.xml',
+    ],
+}
+
