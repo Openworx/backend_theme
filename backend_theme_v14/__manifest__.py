0e10d5249262f35e52112cfd0ce6306a6addf65c
Mario Gielissen <mario@gielissen.org>
2021-03-23 10:48:39 +0100

    web_responsive is merged in 14.0

--- backend_theme_v14/__manifest__.py
+++ backend_theme_v14/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Openworx Material Backend Theme V14",
     "summary": "Openworx Material Backend Theme V14",
-    "version": "14.0.0.1",
+    "version": "14.0.0.2",
     "category": "Theme/Backend",
     "website": "https://www.openworx.nl",
 	"description": """
