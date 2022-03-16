8c0b725f75a2df3b3d5185ebb5ce882d7c6d410c
nottegeo <gn@agenterp.com>
2022-03-16 15:47:29 +0100

    fix sidebar toggle timing bug

--- backend_theme_v14/static/src/js/sidebar-toggle.js
+++ backend_theme_v14/static/src/js/sidebar-toggle.js
@@ -1,7 +1,7 @@
 /* Copyright 2017 Openworx.
  * License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl). */
 
-odoo.define("backend_theme_v11.sidebar-toggle", function (require) {
+odoo.define("backend_theme_v14.sidebar-toggle", function (require) {
   "use strict";
 
   var base = require("web_editor.base");
